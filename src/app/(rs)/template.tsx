71f8c6fdc76d3d34ea7ef5e725dc78fe168fd19b
Yogurt7v <egorov-2k@yandex.ru>
2025-01-14 21:13:19 +0300

    added some routes

---
+++ src/app/(rs)/template.tsx
@@ -0,0 +1,3 @@
+export default function RSTemplate({ children }: { children: React.ReactNode }) {
+  return <div className="animate-appear">{children}</div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,12 @@ export default function Home() {
   return (
     <div className="bg-black bg-home-img bg-cover bg-center">
       <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
-        <div className="flex flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl">
-          <h1>Simple Card</h1>
-          <h2>Repair Shop</h2>
+        <div
+          className="flex flex-col gap-6 p-12 rounded-xl
+         bg-black/90 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl"
+        >
+          <h1 className="text-4xl font-bold">Simple Card</h1>
+          <h2 className="text-3xl font-bold">Repair Shop</h2>
           <address>
             192000 Saint Petersburg <br />
             Galernya st. 33. office 312
