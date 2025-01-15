225e46f32f03272d321695c0f6e13857f322a618
Yogurt7v <egorov-2k@yandex.ru>
2025-01-15 21:29:29 +0300

    metadata practice

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,22 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className=" px-2 w-full">
+      <div className="mx-auto py-4 flex flex-col justify-center items-center gap-4">
+        <h2 className="text-2xl">Page not found</h2>
+        <Link href="/">
+          <Image
+            className="m-0 rounded-xl"
+            src="/images/404.jpg"
+            width={400}
+            height={300}
+            sizes="300px"
+            alt="404"
+          />
+        </Link>
+      </div>
     </div>
   );
 }
