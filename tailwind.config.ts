5a8155938024ba0bded073b502bfb03ca3a5567f
Yogurt7v <egorov-2k@yandex.ru>
2025-01-15 20:26:37 +0300

    slide effect added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { transform } from "next/dist/build/swc/generated-native";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -58,6 +59,29 @@ export default {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)'
+			},
+			keyframes: {
+				"appear": {
+					from: {
+						opacity: "0",
+
+					},
+					to: {
+						opacity: "1",
+					}
+				},
+				"slide": {
+					from: {
+						"transform": "translateX(100%)"
+					},
+					to: {
+						"transform": "translateX(0%)"
+					}
+				}
+			},
+			animation: {
+				appear: "appear 1s ease-in-out",
+				slide: "slide 750ms ease-in-out"
 			}
 		}
 	},
